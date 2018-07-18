792814bdff301434862b28343f862447640a937a
TheDeveloperBot <39490139+TheDeveloperBot@users.noreply.github.com>
2018-07-18 04:32:39 +0400

    Update draw.js

--- source/commands/draw.js
+++ source/commands/draw.js
@@ -16,7 +16,7 @@ module.exports = class extends Command {
 
         const channel = match[1] ? message.guild.channels.get(match[1]) || message.channel : message.channel;
 
-        if (this.client.giveawayCache.filter(giveaway => giveaway.channel.id === channel.id).size <= 0) return message.channel.send(`There is no currently running giveaway in **${channel.name}**. Perhaps you meant \`mg!redraw\`?`);
+        if (this.client.giveawayCache.filter(giveaway => giveaway.channel.id === channel.id).size <= 0) return message.channel.send(`There is no currently running giveaway in **${channel.name}**. Perhaps you meant \`g?redraw\`?`);
 
         const giveaway = this.client.giveawayCache.get(`${channel.id}-${match[2]}`);
         if(!giveaway) return message.channel.send(`ERR: No giveaway is occuring in the specified channel with that ID.`);
